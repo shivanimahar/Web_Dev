38c25ca309478594cec59b6f15c908b28ef860aa
shivanimahar <shivanimahar.offc@gmail.com>
2025-11-05 21:32:59 +0530

    css

---
+++ JavaScript/Chapter1/new.js
@@ -0,0 +1,4 @@
+console.log("hello world")
+console.log("hey i am log...")
+console.log("hey i am core...")
+console.log("hey i am sole...")
---
+++ JavaScript/Chapter1/script.js
@@ -0,0 +1,18 @@
+alert("hello world");
+console.log("hey i am console...")
+console.log("hey i am log...")
+console.log("hey i am core...")
+console.log("hey i am sole...")
+
+var a = prompt("enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+console.log("Your number is " + a)
+document.title = "Hey I am good"
+// document.body.style.backgroundColor = "red"
